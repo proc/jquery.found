d08379dcc4f5cbb06640b9c6fd3bdd138310fd23
Peter Tran <proc.com@gmail.com>
2012-05-01 00:22:12 -0700

    Whitespace cleanup

--- jquery.found.js
+++ jquery.found.js
@@ -3,7 +3,7 @@
  * Peter Tran 
  * http://github.com/proc
  */
- 
+
 ;(function( $ ){
 
   $.fn.found = function( options ) {  
@@ -12,17 +12,17 @@
         title : 'Reset'
       }
       , settings = $.extend(default_options, options);
-    
+
     function calculate_left(reset_element) {
       var width = reset_element.width(),
           left  = -(width * 2);
       return left;
     };
-    
+
     function clear_input($element) {
       $element.val('');
     };
-    
+
     return this.each(function() {
       var $this = $(this);
       if( !$this.data('jquery.found:initialized')) {
@@ -30,7 +30,7 @@
             'style':'position:relative; display: none;',
             'title':settings.title }).html('x'),
             wrapper = $('<div />', { 'style' : 'position:absolute;', 'class' : 'found-input-wrapper' });
-          
+
         function change_handler(element) {
           if( element.val() != '' ) {
             reset_link.show();
@@ -38,29 +38,28 @@
             reset_link.hide();
           }
         };
-      
+
         $this.wrap(wrapper);
         $this.after(reset_link);
         $this.css('width', $this.width() - calculate_left(reset_link));
         $this.css('padding-right', (reset_link.width() * 2) + 'px');
-      
         reset_link.css('left', calculate_left(reset_link) + 'px');
       
         reset_link.bind('click', function() {
           clear_input($this);
           $this.trigger('changed');
         });
-        
+
         $this.attr('placeholder', settings.placeholder);
-        
+
         $this.bind('cut paste keyup change', function() {
           $this.trigger('changed');
         });
-      
+
         $this.bind('changed', function(event) {
           change_handler($(event.target));
         });
-      
+
         $this.trigger('changed');
         $this.data('jquery.found:initialized', true);
       }
